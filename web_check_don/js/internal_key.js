8a51c06f67a84cdd29a9d3c9b96b6f01c1556e18
nguyentrung56789 <47946738+nguyentrung56789@users.noreply.github.com>
2025-10-19 17:04:55 +0700

    Update internal_key.js

--- web_check_don/js/internal_key.js
+++ web_check_don/js/internal_key.js
@@ -1,24 +1,12 @@
 // ======================== internal_key.js ========================
-
 // 1️⃣ Khóa nội bộ (header x-internal-key)
 window.getInternalKey = () => "Trung@123";
 
 // 2️⃣ Cấu hình LOCAL Supabase (offline test)
-const LOCAL_SUPABASE_CONFIG = {
-  url: "https://cywtgdtsxajczljspwxe.supabase.co",
-  anon: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN5d3RnZHRzeGFqY3psanNwd3hlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY3MzI1NjQsImV4cCI6MjA3MjMwODU2NH0.FZ6z6kfUWyf8l7WnA5J1wkrAy7KjpU6VT65EdyXCka8"
-};
+const LOCAL_SUPABASE_CONFIG = { url: "", anon: "" };
 
 // 3️⃣ Cấu hình MAP (Apps Script + Sheet)
-const LOCAL_APP_MAP = {
-  APPS_URL: "https://script.google.com/macros/s/AKfycbxvwPYBOGUyex1ZOgM3E4g2sKMcz3QLao8DaiZz4oRJmnMOwFdF0M30fQD_QR2ubzcK/exec",
-  SHEET_ID: "18YC3kOwKLLvbzYeuXbZ-5U348EV_hAY2Y3wdot42P1c",
-  SHARED_SECRET: "t12345",
-  CSV_URL : "https://docs.google.com/spreadsheets/d/e/2PACX-1vQFLOQCFAQqdcQLP4Yxy0IAVk2f1GCs3nTpEdrITr5s47wOAdViQ3K0VkcQLQSRoLehUe8jFfXrvjkm/pub?output=csv",
-};
-
-// 4️⃣ Webhook nội bộ (ẩn khỏi body JSON)
-const LOCAL_WEBHOOK = "https://dhsybbqoe.datadex.vn/webhook/hoadon";
+const LOCAL_APP_MAP = { APPS_URL: "", SHEET_ID: "", SHARED_SECRET: "", CSV_URL: "" };
 
 
 // Cho phép các file khác gọi giống như `url` và `anon`
